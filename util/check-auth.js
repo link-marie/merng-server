3929271efbc1f5b12a37ffcc6560101c324d8b27
link-marie <rijkmarie@gmail.com>
2020-05-05 17:42:16 +0900

    Use constant

--- util/check-auth.js
+++ util/check-auth.js
@@ -1,11 +1,11 @@
 const { AuthenticationError } = require('apollo-server')
 const jwt = require('jsonwebtoken')
-const { SECRET_KEY } = require('../config')
+const { SECRET_KEY, strBearer } = require('../config')
 
 module.exports = ( context) => {
   const authHeader = context.req.headers.authorization
   if( authHeader){
-    const token = authHeader.split('Bearer ')[1]
+    const token = authHeader.split(`${strBearer} `)[1]
     if( token){
       try{
         const user = jwt.verify(token, SECRET_KEY)
@@ -15,7 +15,7 @@ module.exports = ( context) => {
         throw new AuthenticationError('Invalid/Expired token')
       }
     }
-    throw new Error('Authentication token must be \'Bearer [token]')
+    throw new Error(`Authentication token must be \'${strBearer} [token]\'`)
   }
   throw new Error('Authorization header must be provided')
 }

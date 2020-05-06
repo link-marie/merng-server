fe723a974e02580ec7f368546eb92e7e27cca5a2
link-marie <rijkmarie@gmail.com>
2020-05-06 16:37:19 +0900

    Fix strBearer

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGODB: 'mongodb+srv://user001:MUeYGn8TrtYXxgOB@cluster0-50oox.mongodb.net/merng?retryWrites=true&w=majority',
   SECRET_KEY: 'some very secret key',
-  strBearer: 'Beare',
+  strBearer: 'Bearer',
 }
